9af97e0bd3c157d2cbd772ea0e8cb14e86ab8374
Benjamin Bean <benjamin.bean@comcast.net>
2018-01-28 23:44:01 -0700

    successfully downloading files
    multithreading in python was way more exciting than I had anticipated

--- downloadImages/ImageDownloader.py
+++ downloadImages/ImageDownloader.py
@@ -1,25 +1,169 @@
+from pathlib import Path
+import threading
+from collections import deque
+
+import requests
+
 from imagenet_fall11_urls.URLsParser import URLsParser
 
 class ImageDownloader:
-    urls_path = ""
-    dest_path = ""
-    downloaded_map_path = ""
+    dest_dirname = ""
+    dest_path = None
+    dl_urls_filename = ""
+    dl_urls_file = None
+    urls_collection = deque()
+    threads_to_urls = {}
+    max_dl_count = None
+    dl_count = 0
+    dl_remaining = 0
+    dl_total = 0
+    lock = threading.Lock()
 
-    def __init__(self, urls_path, dest_path, downloaded_map_path):
-        self.urls_path = urls_path
-        self.dest_path = dest_path
-        self.downloaded_map_path = downloaded_map_path
+    def __init__(self, dest_dirname, dl_urls_filename):
+        self.dest_dirname = dest_dirname
+        self.dl_urls_filename = dl_urls_filename
 
-    def download(self, matcher, count):
-        urls_parser = URLsParser(self.urls_path, notify_interval=(10**6))
-        matched = 0
-        retval = []
+    def download(self, urls_collection, max_dl_count=None):
+        # create the destination directory(s) as needed
+        self.dest_path = Path(self.dest_dirname)
+        dl_urls_path = Path(self.dl_urls_filename)
+        self.dest_path.mkdir(parents=True, exist_ok=True)
+        dl_urls_path.parent.mkdir(parents=True, exist_ok=True)
+        dl_urls_path.touch()
+        self.max_dl_count = max_dl_count
+        self.dl_count = 0
 
-        for group in urls_parser:
-            if (matcher(group[0])):
-                retval.append([urls_parser.line_num, *group])
-                matched += 1
-                if (matched >= count):
-                    break
+        with dl_urls_path.open(mode='r+') as self.dl_urls_file:
+            # read the current contents of the dl_urls_file
+            # (to know where to resume downloading from the last point)
+            dl_urls = self.__read_dl_urls_file(self.dl_urls_file)
+            self.dl_total = len(dl_urls)
+            print(f"found {self.dl_total} already download urls")
+            self.dl_remaining = (self.max_dl_count - self.dl_total) if (self.max_dl_count != None) else None
 
-        return retval
+            # retain all urls that have not yet been loaded
+            self.urls_collection = deque()
+            for url in urls_collection:
+                if not url in dl_urls:
+                    self.urls_collection.append(url)
+
+            # start downloading!
+            threads = []
+            for i in range(3):
+                helper = ImageDownloadHelper(self, f"T{i}", threading.current_thread())
+                threads.append(helper)
+                threads[-1].start()
+
+            # wait for downloads to finish
+            for t in threads:
+                while t.isAlive():
+                    t.join(timeout=1)
+
+        return self.dl_count
+    
+    def __read_dl_urls_file(self, dl_urls_file):
+        dl_urls = set()
+        for line in dl_urls_file:
+            line = line.strip()
+            if (len(line) > 0):
+                dl_urls.add(line)
+        return dl_urls
+
+    def get_next_url(self, thread):
+        self.lock.acquire()
+
+        try:
+            # check if we need more downloads
+            if (self.dl_remaining != None and self.dl_remaining <= 0):
+                raise IndexError("no more downloads remaining")
+            self.dl_remaining -= 1
+
+            # retrieve the next url
+            url = self.urls_collection.pop()
+            self.threads_to_urls[thread] = url
+            return url
+        except IndexError as e:
+            try:
+                del self.threads_to_urls[thread]
+            except (KeyError, IndexError):
+                pass
+            raise e
+        finally:
+            self.lock.release()
+
+    def dl_completed(self, thread, img_data):
+        # get the url
+        url = self.threads_to_urls[thread]
+        del self.threads_to_urls[thread]
+
+        try:
+            # save the file
+            img_extension = url.split(".")[-1]
+            self.lock.acquire()
+            img_name = f"{self.dl_total}.{img_extension}"
+            self.lock.release()
+            img_path = self.dest_path.joinpath(img_name)
+            with img_path.open('wb') as handler:
+                handler.write(img_data)
+
+            # note that the file has been saved
+            self.lock.acquire()
+            self.dl_urls_file.write(url + "\n")
+            self.dl_count += 1
+            self.dl_total += 1
+        finally:
+            self.lock.release()
+
+        if (self.dl_count % 10 == 0):
+            print(f"downloaded {self.dl_count:,} images...")
+
+    def dl_failed(self, thread, error):
+        # get the url
+        url = self.threads_to_urls[thread]
+        del self.threads_to_urls[thread]
+        print(f"failed to download image \"{url}\": {error}")
+
+        # update the needed count
+        if (self.dl_remaining != None):
+            self.dl_remaining += 1
+
+class ImageDownloadHelper(threading.Thread):
+    image_downloader = None
+    parent_thread = None
+
+    def __init__(self, image_downloader, name, parent_thread):
+        self.image_downloader = image_downloader
+        self.parent_thread = parent_thread
+        super().__init__(name=name)
+
+    def run(self):
+        while True:
+            # get the image url to download
+            try:
+                url = self.image_downloader.get_next_url(self)
+            except IndexError as e:
+                break
+
+            # download the image
+            try:
+                request = requests.get(url)
+                img_data = request.content
+                if (request.status_code != 200):
+                    raise NoDataException(f"status code {request.status_code}")
+                if (len(img_data) < 10):
+                    raise NoDataException("No data")
+                if (len(img_data) == 2051):
+                    raise NoDataException("Flickr 'this image is no longer available' image")
+                if (img_data[0:9].decode("utf-8", "replace") == "<!DOCTYPE"):
+                    raise NotAnImageException("not an image")
+                if not self.parent_thread.is_alive():
+                    return
+                self.image_downloader.dl_completed(self, img_data)
+            except Exception as e:
+                self.image_downloader.dl_failed(self, e)
+
+class NoDataException(Exception):
+    pass
+
+class NotAnImageException(Exception):
+    pass
--- main.py
+++ main.py
@@ -7,15 +7,30 @@ def matches(prefix_parser, value):
     does_match = prefix_parser.matches_prefix(prefix)
     return does_match
 
+def get_matching_urls(urls_path, matcher, count):
+    urls_parser = URLsParser(urls_path, notify_interval=(10**6))
+    matched = 0
+    retval = []
+
+    for group in urls_parser:
+        if (matcher(group[0])):
+            retval.append(group[1])
+            matched += 1
+            if (matched >= count):
+                break
+
+    return retval
+
 if __name__ == "__main__":
     urls_path = "imagenet_fall11_urls/fall11_urls.txt"
-    dest_path = "downloadImages/downloads/people"
+    dest_path = "downloadImages/downloads/person"
     downloaded_map_path = "downloadImages/downloads/people_map.txt"
-    prefix_parser = PrefixParser("imagenet_fall11_urls/people.txt")
+    prefix_parser = PrefixParser("imagenet_fall11_urls/person.txt")
     prefix_parser.parse()
     matcher = lambda s: matches(prefix_parser, s)
-    count = 10**6
+    count = 1000
 
-    downloader = ImageDownloader(urls_path, dest_path, downloaded_map_path)
-    matches = downloader.download(matcher, count)
-    print(f"found {len(matches):,} people images")
+    downloader = ImageDownloader(dest_path, downloaded_map_path)
+    matching_urls = get_matching_urls(urls_path, matcher, count * 2)
+    download_count = downloader.download(matching_urls, count)
+    print(f"downloaded {download_count:,} person images")
